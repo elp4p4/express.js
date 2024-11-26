69ffbadc27e7b52a4d6817a55c8178269dcfac3d
Ahmed <brazfu145@gmail.com>
2024-11-26 14:54:02 +0100

    middleware

--- app.js
+++ app.js
@@ -2,26 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-// Middleware
-const workingHoursMiddleware = (_req, res, next) => {
-  const now = new Date();
-  const day = now.getDay();
-  const hour = now.getHours();
-
-  if (day >= 1 && day <= 5 && hour >= 9 && hour < 17) {
-    next();
-  } else {
-    res.status(403).sendFile('error.html', { root: app.get('pages') });
-  }
-};
-
-
-// Apply middleware for all routesss
-app.use(workingHoursMiddleware);
-
-// Define routes for the pages
+// Set the pages directory for serving HTML files
 app.set('pages', 'pages');
 
+// Apply the middleware globally
+const verify = require("./middleware/verify");
+app.use(verify(app.get('pages')));
+
+// Define routes for the pages
 app.get('/', (_req, res) => {
   res.sendFile('home.html', { root: app.get('pages') });
 });
